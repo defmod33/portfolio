ec5c4115eeeacc903c2c6eb3de3ffe473f145309
defmod33 <defmod33@gmail.com>
2013-09-26 13:07:52 -0700

    initial product commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+<meta charset="UTF-8" />
+
+<title>Lawrence Forsythe</title>
+</head>
+
+<body>
+	<div class="wrapper">
+		<header class="extendFull">
+			
+		</header>
+			<div class="main">
+				
+			</div>
+		<footer>
+			
+		</footer class="extendFull">
+	</div>
+</body>
+</html>
